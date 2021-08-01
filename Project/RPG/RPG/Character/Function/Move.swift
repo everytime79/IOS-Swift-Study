e966f0d0fdb227ac181360fb70946689b06d98c8
everytime79 <everytime79@naver.com>
2021-08-01 21:14:47 +0900

    Update Move.swift

--- Project/RPG/RPG/Character/Function/Move.swift
+++ Project/RPG/RPG/Character/Function/Move.swift
@@ -66,72 +66,14 @@ extension Character {
             SpeedX = -SP
             SpeedY = 0
             
-            // 동쪽으로 가는 동안,
-            if CurrentDirection != LastDirection {
-                
-                let MoveAtlas = SKTextureAtlas(named: "RedSwanMoveE")
-                var MoveFrame = [SKTexture]()
-                
-                //반복 실행
-                for i in 1...MoveAtlas.textureNames.count / 2 {
-                    //1부터 4까지
-                    let TextureName = "RedSwanMoveE" + "\(i)" //i는 숫자임으로 문자로 바꿔준다.
-                    MoveFrame .append(MoveAtlas.textureNamed(TextureName))
-                }
-                
-                let MoveAnimationAction = SKAction.repeatForever(SKAction.animate(with: MoveFrame, timePerFrame: 0.125))
-                // repeat한다. with 무브프레임, 바뀌는 시간 0.125
-                self.run(MoveAnimationAction, withKey: "Move")
-                // 캐릭터가 실행, 그리고 이 액션의 이름은 Move
-            }
-            LastDirection = CurrentDirection
-            
         case .S:
             SpeedX = 0
             SpeedY = -SP
-            
-            // 동쪽으로 가는 동안,
-            if CurrentDirection != LastDirection {
-                
-                let MoveAtlas = SKTextureAtlas(named: "RedSwanMoveE")
-                var MoveFrame = [SKTexture]()
-                
-                //반복 실행
-                for i in 1...MoveAtlas.textureNames.count / 2 {
-                    //1부터 4까지
-                    let TextureName = "RedSwanMoveE" + "\(i)" //i는 숫자임으로 문자로 바꿔준다.
-                    MoveFrame .append(MoveAtlas.textureNamed(TextureName))
-                }
-                
-                let MoveAnimationAction = SKAction.repeatForever(SKAction.animate(with: MoveFrame, timePerFrame: 0.125))
-                // repeat한다. with 무브프레임, 바뀌는 시간 0.125
-                self.run(MoveAnimationAction, withKey: "Move")
-                // 캐릭터가 실행, 그리고 이 액션의 이름은 Move
-            }
-            LastDirection = CurrentDirection
             
         case .N:
             SpeedX = 0
             SpeedY = SP
 
-            if CurrentDirection != LastDirection {
-                
-                let MoveAtlas = SKTextureAtlas(named: "RedSwanMoveE")
-                var MoveFrame = [SKTexture]()
-                
-                for i in 1...MoveAtlas.textureNames.count / 2 {
-
-                    let TextureName = "RedSwanMoveE" + "\(i)"
-                    MoveFrame .append(MoveAtlas.textureNamed(TextureName))
-                }
-                
-                let MoveAnimationAction = SKAction.repeatForever(SKAction.animate(with: MoveFrame, timePerFrame: 0.125))
-
-                self.run(MoveAnimationAction, withKey: "Move")
-
-            }
-            LastDirection = CurrentDirectio
-            
         }
         
         self.position = CGPoint(x: self.position.x + SpeedX, y: self.position.y + SpeedY)
