08a41dec716b15fd7fc917998f25f83e260c6fd6
everytime79 <everytime79@naver.com>
2021-08-03 21:40:57 +0900

    Update Meteor.swift

--- Project/RPG/RPG/Character/Attack/Meteor.swift
+++ Project/RPG/RPG/Character/Attack/Meteor.swift
@@ -6,7 +6,11 @@ extension Character {
     
     func Attack_Meteor() {
         
-        
+        let Position = [CGPoint(x: 0, y: 200),CGPoint(x: 150, y: 100),
+                        CGPoint(x: -150, y: 100),CGPoint(x: 100, y: 0),
+                        CGPoint(x: 200, y: 0),CGPoint(x: 0, y: -200),
+                        CGPoint(x: 150, y: -100),CGPoint(x: -150, y: -100),
+                        CGPoint(x: -100, y: 0),CGPoint(x: -200, y: 0)]
         
     }
     
