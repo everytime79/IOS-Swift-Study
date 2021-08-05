f1f985e1e80f467bd57841eeb645b2736243b06c
everytime79 <everytime79@naver.com>
2021-08-05 22:42:43 +0900

    Update Share.swift

--- Project/RPG/RPG/Supporting File/Share.swift
+++ Project/RPG/RPG/Supporting File/Share.swift
@@ -1,13 +1,13 @@
 import Foundation
 import SpriteKit
 
-// MARK: - Variable
+// MARK: - Variable - 화면 카메라
 var Scene = GameScene()
 let LocalCamera = SKCameraNode()
 
 // MARK: - Enumeration
 
-// Direction //
+// Direction // - 방향
 enum Direction {
     case E, W, S, N, ST
 }
