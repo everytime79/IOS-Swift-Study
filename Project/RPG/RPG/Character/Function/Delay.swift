bd6a753ea577f4344a6c2c7c742ed03c598ee19c
everytime79 <everytime79@naver.com>
2021-08-08 21:52:40 +0900

    Update Delay.swift

--- Project/RPG/RPG/Character/Function/Delay.swift
+++ Project/RPG/RPG/Character/Function/Delay.swift
@@ -9,6 +9,8 @@ extension Character {
         // Circle
         let Radius = CGFloat(46.6)
         let Circle = SKShapeNode(circleOfRadius: Radius)
+        // 스킬 버튼 보다는 위에 만들어줘야 하기 때문에 (버튼 : 100) -> 101로 셋팅
         Circle.zPosition = 101
+        
     }
 }
