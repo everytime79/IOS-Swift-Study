802d9ecd45e4e7314bfa93e27917faf4bbf67f8b
everytime79 <everytime79@naver.com>
2021-08-08 21:51:47 +0900

    Update Delay.swift

--- Project/RPG/RPG/Character/Function/Delay.swift
+++ Project/RPG/RPG/Character/Function/Delay.swift
@@ -1,1 +1,14 @@
+import SpriteKit
+import GameplayKit
 
+extension Character {
+    
+    // 사용하려는 스킬의 쿨타임을 추가
+    func Function_DelayEffect(Button: String, Delay: CGFloat) {
+        
+        // Circle
+        let Radius = CGFloat(46.6)
+        let Circle = SKShapeNode(circleOfRadius: Radius)
+        Circle.zPosition = 101
+    }
+}
