dcfdb7487872b0ef595024cd5188f069bd937443
everytime79 <everytime79@naver.com>
2021-08-18 21:32:34 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -18,6 +18,12 @@ class Character: SKSpriteNode {
     var SpeedX = CGFloat()
     var SpeedY = CGFloat()
     
+    // Mode //
+    var MoveDelayIs = Bool(false) // 값이 2개. 참 거짓 트루 폴스-> 딜레이가 있는가 없는가,
+    var AttackDelayIs = Bool(false)
+    var SkillDelayIs = Bool(false)
+    
+    
 // Variable_End
 
 
