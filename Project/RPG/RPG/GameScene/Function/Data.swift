3864dade411d75d4e7af582a194b0afa2d8ee1a0
everytime79 <everytime79@naver.com>
2021-08-18 21:56:16 +0900

    Update Data.swift

--- Project/RPG/RPG/GameScene/Function/Data.swift
+++ Project/RPG/RPG/GameScene/Function/Data.swift
@@ -4,7 +4,18 @@ import GameplayKit
 extension GameScene {
     
 // MARK: - Player
-    
+    func Data_Player() {
+        
+    }
 // Player_End
     
 }
+
+
+
+
+
+// Player //
+Player.zPosition = 10
+self.addchild(Player)
+
