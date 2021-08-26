eec830627ed2552ef606aa0005e0457473dac37a
everytime79 <everytime79@naver.com>
2021-08-26 19:33:47 +0900

    Update Data.swift

--- Project/RPG/RPG/GameScene/Function/Data.swift
+++ Project/RPG/RPG/GameScene/Function/Data.swift
@@ -22,19 +22,19 @@ extension GameScene {
             
             let MonsterGroupData = GameData["MonsterGroup"] as! [String:Any]
             
-            for i in MonsterGroupData {
+            for (_, value) in MonsterGroupData {
                 
+                
+                let Data = GameData["Player"] as! [String:Any]
+                let SP = Data["SP"] as! CGFloat
+                
+                Player = Character(imageNamed: "RedSwan")
+                Player.SP = SP
+                Player.zPosition = 10
+                self.addChild(Player)
+
                 
             }
-            
-            let Data = GameData["Player"] as! [String:Any]
-            let SP = Data["SP"] as! CGFloat
-            
-            Player = Character(imageNamed: "RedSwan")
-            Player.SP = SP
-            Player.zPosition = 10
-            self.addChild(Player)
-
         }
     // Monster_End
 }
