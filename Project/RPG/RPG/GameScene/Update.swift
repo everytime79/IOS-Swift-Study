d9b0102df5016c3c3f7761016c0f75dbbc62ba57
everytime79 <everytime79@naver.com>
2021-08-29 01:28:51 +0900

    Update Update.swift

--- Project/RPG/RPG/GameScene/Update.swift
+++ Project/RPG/RPG/GameScene/Update.swift
@@ -11,6 +11,21 @@ extension GameScene {
         Player.Function_Move()
         
         // MiniMap
+        PlayerMinion.position = CGPoint.zero
+        
+        for i in 0...MonsterGroup.count - 1 {
+            
+            MonsterMinionGroup[i].position.x = (MonsterGroup[i].position.x - Player.position.x) / 10
+            MonsterMinionGroup[i].position.y = (MonsterGroup[i].position.y - Player.position.y) / 10
+            
+            if MiniMap.intersects(MonsterMinionGroup[i]) {
+                
+                MonsterMinionGroup[i].alpha = 1
+            } else {
+                
+                MonsterMinionGroup[i].alpha = 0
+            }
+        }
         
         // Camera
         LocalCamera.position = Player.position
