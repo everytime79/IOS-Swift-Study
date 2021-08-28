45dd2698a2af15a83e86d00e404594b9f801eb8d
everytime79 <everytime79@naver.com>
2021-08-29 00:19:21 +0900

    Update PhysicsBody.swift

--- Project/RPG/RPG/Character/Function/PhysicsBody.swift
+++ Project/RPG/RPG/Character/Function/PhysicsBody.swift
@@ -5,6 +5,9 @@ extension Character {
     
 // MARK: - Player
     func PhysicsBody_Player() {
+    
+        let Texture = SKTexture(imageNamed: "RedSwan")
+        let PhysicsBody = SKPhysicsBody(texture: Texture, size: self.size)
         
     }
     
