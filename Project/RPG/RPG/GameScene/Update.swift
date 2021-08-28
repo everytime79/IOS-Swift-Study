8e862f621138363604864fbd24dab240c6a00099
everytime79 <everytime79@naver.com>
2021-08-29 01:25:37 +0900

    Update Update.swift

--- Project/RPG/RPG/GameScene/Update.swift
+++ Project/RPG/RPG/GameScene/Update.swift
@@ -6,9 +6,13 @@ extension GameScene {
 // MARK: - Update
     override func update(_ currentTime: TimeInterval) {
         
+        
+        // Player
         Player.Function_Move()
         
-        //
+        // MiniMap
+        
+        // Camera
         LocalCamera.position = Player.position
     }
 // Update_End
