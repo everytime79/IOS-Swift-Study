c461ac48a4f84afe9dddf116c4c3f2658da339f6
everytime79 <everytime79@naver.com>
2021-08-29 00:18:26 +0900

    Update PhysicsBody.swift

--- Project/RPG/RPG/Character/Function/PhysicsBody.swift
+++ Project/RPG/RPG/Character/Function/PhysicsBody.swift
@@ -2,3 +2,14 @@ import SpriteKit
 import GameplayKit
 
 extension Character {
+    
+// MARK: - Player
+    func PhysicsBody_Player() {
+        
+    }
+    
+    
+// MARK: - Monster
+    
+    
+}
