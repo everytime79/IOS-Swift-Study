a818f474e26c2643705704862f62c14d002a795d
everytime79 <everytime79@naver.com>
2021-09-05 07:48:34 +0900

    Add CharacterUpdate

--- Project/RPG/RPG/Character/Function/AI.swift
+++ Project/RPG/RPG/Character/Function/AI.swift
@@ -36,6 +36,8 @@ extension Character {
                 
             }
         }
+        
+        Function_Move()
     }
     
 }
---
+++ Project/RPG/RPG/Character/Function/CharacterUpdate.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterUpdate.swift
+//  RPG
+//
+//  Created by 박성수 on 2021/09/05.
+//
+
+import Foundation
