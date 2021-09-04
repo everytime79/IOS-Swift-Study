1580c5177242cd3a8910583c004f6349cd82276e
everytime79 <everytime79@naver.com>
2021-09-05 07:44:30 +0900

    Update AI.swift

--- Project/RPG/RPG/Character/Function/AI.swift
+++ Project/RPG/RPG/Character/Function/AI.swift
@@ -12,6 +12,11 @@ extension Character {
             
             let WaitAction = SKAction.wait(forDuration: 2)
             let StopAction = SKAction.run { self.CurrentDirection = .ST }
+            let StopDelayAction = SKAction.run { self.StopDelayIs = false }
+            
+            self.run(SKAction.sequence([WaitAction, StopAction, WaitAction, StopDelayAction]))
+            
+            
         }
     }
     
