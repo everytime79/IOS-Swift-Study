e7189e4288c79909009fd3f4f38bd08ba623f373
everytime79 <everytime79@naver.com>
2021-09-05 07:50:02 +0900

    Update CharacterUpdate.swift

--- Project/RPG/RPG/Character/Function/CharacterUpdate.swift
+++ Project/RPG/RPG/Character/Function/CharacterUpdate.swift
@@ -3,7 +3,21 @@ import GameplayKit
 
 extension Character {
     
-    
+// MARK: - Monster
+    func Update_Monster() {
+        
+        // Status
+        if self.WalkingIs == true {
+            
+            self.SP = WalkSP
+            
+        } else {
+            
+            self.SP = RunSP
+            
+        }
+        
+    }
     
     
 }
