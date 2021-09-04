5aab1dfa310850323f4c759104411c0584587ffd
everytime79 <everytime79@naver.com>
2021-09-05 07:41:17 +0900

    Update AI.swift

--- Project/RPG/RPG/Character/Function/AI.swift
+++ Project/RPG/RPG/Character/Function/AI.swift
@@ -4,6 +4,12 @@ import GameplayKit
 extension Character {
 
 // MARK: - Random Walk
-    
+    func AI_RandomWalk() {
+        
+        if StopDelayIs == false {
+            
+            StopDelayIs = true
+        }
+    }
     
 }
