0e96ea6b0283fc8bd8b06003da2b4ea5c6f7718a
everytime79 <everytime79@naver.com>
2021-09-05 07:46:44 +0900

    Update AI.swift

--- Project/RPG/RPG/Character/Function/AI.swift
+++ Project/RPG/RPG/Character/Function/AI.swift
@@ -16,7 +16,25 @@ extension Character {
             
             self.run(SKAction.sequence([WaitAction, StopAction, WaitAction, StopDelayAction]))
             
+            let RandomNumber = arc4random_uniform(4) // 방향이 4가지 밖에 없기 때문에,
             
+            if RandomNumber == 0 {
+                
+                self.CurrentDirection = .E
+                
+            } else if RandomNumber == 1 {
+                
+                self.CurrentDirection = .W
+                
+            } else if RandomNumber == 2 {
+                
+                self.CurrentDirection = .S
+                
+            } else if RandomNumber == 3 {
+                
+                self.CurrentDirection = .N
+                
+            }
         }
     }
     
