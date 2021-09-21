eaede76ac69913ed97712407c5b15af59801738f
everytime79 <everytime79@naver.com>
2021-09-21 11:27:25 +0900

    Update Standoff.swift

--- Project/RPG/RPG/Character/Attack/Standoff.swift
+++ Project/RPG/RPG/Character/Attack/Standoff.swift
@@ -1,8 +1,48 @@
-//
-//  Standoff.swift
-//  RPG
-//
-//  Created by 박성수 on 2021/09/21.
-//
+import SpriteKit
+import GameplayKit
 
-import Foundation
+extension Character {
+  
+    func Attack_Standoff(){
+        
+        let AttackBody = Character(imageNamed: "Attack_StandoffE")
+        AttackBody.zPosition = 1
+        
+        self.addChild(AttackBody)
+        Attack_Standoff_Animation(AttackBody: AttackBody)
+        
+        // Action -> 어택에 대한 애니메이션
+        let WaitAction = SKAction.wait(forDuration: 0.2)
+        let RemoveAction = SKAction.run { AttackBody.removeFromParent()}
+        //WaitAction 이 끝나야, Remove 하도록 sequence
+        AttackBody.run(SKAction.sequence([WaitAction, RemoveAction]))
+        
+        // Delay -> 딜레이에 대한 애니메이션
+        let MoveWaitAction = SKAction.wait(forDuration: 0.2)
+        let MoveDelayAction = SKAction.run { self.MoveDelayIs = false}
+        
+        let AttackWaitAction = SKAction.wait(forDuration: 0.5)
+        let AttackDelayAction = SKAction.run { self.AttackDelayIs = false}
+        
+        Function_DelayEffect(Button: "AttackButton", Delay: 0.5)
+        MoveDelayIs = true
+        AttackDelayIs = true
+        
+        self.run(SKAction.sequence([MoveWaitAction, MoveDelayAction])) // 0.2 초 기다린 후 -> 다시 가능하게
+        self.run(SKAction.sequence([AttackWaitAction, AttackDelayAction])) // 0.5 초 기다린 후 -> 다시 가능하게
+        
+    }
+    
+    func Attack_Standoff_Animation(AttackBody: Character){
+        
+        if self.AttackDirection == .E {
+            
+        } else if self.AttackDirection == .W {
+            
+        } else if self.AttackDirection == .S {
+            
+        } else if self.AttackDirection == .N {
+            
+        }
+    }
+}
