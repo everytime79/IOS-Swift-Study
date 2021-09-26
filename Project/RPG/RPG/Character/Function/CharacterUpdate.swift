741916a8eebe3c9dd00de94396c4b94ff536b20c
everytime79 <everytime79@naver.com>
2021-09-26 18:18:02 +0900

    Update CharacterUpdate.swift

--- Project/RPG/RPG/Character/Function/CharacterUpdate.swift
+++ Project/RPG/RPG/Character/Function/CharacterUpdate.swift
@@ -22,14 +22,32 @@ extension Character {
         
         if DistanceToPlayer < SearchRange {
             
+            // 범위 안에 들어오면 캐릭터를 체이스
             self.WalkingIs = false
             AI_Chase(Target: Scene.Player)
             
         } else {
             
+            // 범위 밖으로 나가면 랜덤 걷기
             self.WalkingIs = true
             AI_RandomWalk()
             
+        }
+        
+        if DistanceToPlayer <= AttackRange{
+            
+            if self.AttackDelayIs == false {
+                
+                if self.Class == "Knigh" {
+                    
+                    Attack_Melee()
+                    
+                } else if self.Class == "Archer" {
+                    
+                    Attack_Standoff()
+                    
+                }
+            }
         }
     }
 }
