251432652f22a7b914cd0ecaa0c0e17ce34a5bbf
everytime79 <everytime79@naver.com>
2021-10-04 23:17:45 +0900

    Update Damage.swift

--- Project/RPG/RPG/Character/Function/Damage.swift
+++ Project/RPG/RPG/Character/Function/Damage.swift
@@ -3,4 +3,41 @@ import GameplayKit
 
 extension Character {
     
+// MARK: - Effect
+    func Damage_Effect(Attacker: Character, Defender: Character) {
+    
+    // Damage Point
+        let AT = Attacker.AT
+        let AR = Attacker.Luck
+        let DF = Defender.DF
+        let DR = Defender.Luck
+        
+        let ATRate = (AT * AR / 100).rounded(.down) // 정수로 떨어지게끔, .rounded(.down) 사용
+        let ATRange = UInt32((AT - ATRate).rounded(.down))
+        let DFRate = (DF * DR / 100).rounded(.down)
+        let DFRange = UInt32((DF - DFRate).rounded(.down))
+        
+        let ATPoint = Int(arc4random_uniform(ATRange + 1)) + Int(ATRate)
+        let DFPoint = Int(arc4random_uniform(DFRange + 1)) + Int(DFRate)
+        
+        let DamagePoint = ATPoint - DFPoint
+        
+    // Damage Label
+        let DamageLabel = SKLabelNode(fontNamed: "04b_19")
+        DamageLabel.zPosition = 13 // 상단에 출력하기 위함
+        DamageLabel.fontSize = 50 // 글자 크기
+        DamageLabel.text = "\(DamagePoint)"
+        
+        if self.Sort == "Player" {
+            
+            DamageLabel.fontColor = UIColor.red
+            
+        } else if self.Sort == "Monster" {
+            
+            DamageLabel.fontColor = UIColor.green
+            
+        }
+        
+
+    }
 }
