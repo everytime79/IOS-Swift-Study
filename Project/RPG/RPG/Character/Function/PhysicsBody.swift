2ea9fe983e1fa46cfe4a8546e65389356bf0c5c4
everytime79 <everytime79@naver.com>
2021-10-04 22:23:53 +0900

    Update PhysicsBody.swift

--- Project/RPG/RPG/Character/Function/PhysicsBody.swift
+++ Project/RPG/RPG/Character/Function/PhysicsBody.swift
@@ -51,15 +51,14 @@ extension Character {
 // MARK: - Monster
     func PhysicsBody_MonsterAttack() {
         
-        let Texture = SKTexture(imageNamed: "Archer")
-        let PhysicsBody = SKPhysicsBody(texture: Texture, size: self.size)
-        
+        let PhysicsBody = SKPhysicsBody(rectangleOf: self.size)
+
         self.physicsBody = PhysicsBody
         self.physicsBody?.isDynamic = true
         self.physicsBody?.affectedByGravity = false // 중력의 영향 x
         self.physicsBody?.allowsRotation = false // 마주쳤을 떄 돌 필요가 없다.
-        self.physicsBody?.categoryBitMask = BodyType.Monster.rawValue // Player = 1, Monster = 2
-        self.physicsBody?.contactTestBitMask = BodyType.PlayerAttack.rawValue
+        self.physicsBody?.categoryBitMask = BodyType.MonsterAttack.rawValue
+        self.physicsBody?.contactTestBitMask = BodyType.Player.rawValue
         self.physicsBody?.collisionBitMask = BodyType.Player.rawValue | BodyType.Monster.rawValue
         // 플레이어와 충돌했을 떄, 중복되지 않겠다.(겹침 x). | 몬스터와도 충돌했을 때, 겹치지 않겠다.(오버랩 되지 않겠다.)
     }
