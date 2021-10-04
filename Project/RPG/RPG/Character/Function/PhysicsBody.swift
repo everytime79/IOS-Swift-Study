c066049a7d97ab550f0cf11cef701d7da7b49986
everytime79 <everytime79@naver.com>
2021-10-04 22:18:49 +0900

    Update PhysicsBody.swift

--- Project/RPG/RPG/Character/Function/PhysicsBody.swift
+++ Project/RPG/RPG/Character/Function/PhysicsBody.swift
@@ -17,6 +17,20 @@ extension Character {
         self.physicsBody?.collisionBitMask = BodyType.Monster.rawValue // Monster하고 겹치지 않겠다.
     }
     
+// MARK: - Player
+    func PhysicsBody_Player() {
+        
+        let Texture = SKTexture(imageNamed: "RedSwan")
+        let PhysicsBody = SKPhysicsBody(texture: Texture, size: self.size)
+            
+        self.physicsBody = PhysicsBody
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.affectedByGravity = false // 중력의 영향 x
+        self.physicsBody?.allowsRotation = false // 마주쳤을 떄 돌 필요가 없다.
+        self.physicsBody?.categoryBitMask = BodyType.Player.rawValue // Player = 1, Monster = 2
+        self.physicsBody?.collisionBitMask = BodyType.Monster.rawValue // Monster하고 겹치지 않겠다.
+    }
+    
 // MARK: - Monster
     func PhysicsBody_Monster() {
     
