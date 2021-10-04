2387d551aaa9f92812f0e337d646cac12c6aa502
everytime79 <everytime79@naver.com>
2021-10-04 22:34:25 +0900

    Update Melee.swift

--- Project/RPG/RPG/Character/Attack/Melee.swift
+++ Project/RPG/RPG/Character/Attack/Melee.swift
@@ -73,12 +73,12 @@ extension Character {
             let RangeAction = SKAction.move(by: CGVector(dx: Distance, dy: 0), duration: 0.2)
 
         } else if self.AttackDirection == .W {
-            let RangeAction = SKAction.move(by: CGVector(dx: Distance, dy: 0), duration: 0.2)
+            let RangeAction = SKAction.move(by: CGVector(dx: -Distance, dy: 0), duration: 0.2)
             
         } else if self.AttackDirection == .S {
-            let RangeAction = SKAction.move(by: CGVector(dx: Distance, dy: 0), duration: 0.2)
+            let RangeAction = SKAction.move(by: CGVector(dx: 0, dy: -Distance), duration: 0.2)
             
         } else if self.AttackDirection == .N {
-            let RangeAction = SKAction.move(by: CGVector(dx: Distance, dy: 0), duration: 0.2)
+            let RangeAction = SKAction.move(by: CGVector(dx: 0, dy: Distance), duration: 0.2)
     }
 }
