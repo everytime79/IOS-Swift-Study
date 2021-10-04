b71c7d6da4d69d7c210ee1c67a49e86907c846b5
everytime79 <everytime79@naver.com>
2021-10-04 22:43:57 +0900

    Update Physics.swift

--- Project/RPG/RPG/GameScene/Physics.swift
+++ Project/RPG/RPG/GameScene/Physics.swift
@@ -5,10 +5,21 @@ extension GameScene {
     
     func didBegin(_ contact: SKPhysicsContact) {
         
-        // MARK: - Attack
+// MARK: - Attack
         
         //Player
         if contact.bodyA.categoryBitMask == BodyType.PlayerAttack.rawValue && contact.bodyB.categoryBitMask == BodyType.Monster.rawValue {
+            
+            print("Player Attack")
+            
+        } else if contact.bodyB.categoryBitMask == BodyType.PlayerAttack.rawValue && contact.bodyA.categoryBitMask == BodyType.Monster.rawValue {
+            
+            print("Player Attack")
+            
+        }
+        
+        //Monster
+        if contact.bodyA.categoryBitMask == BodyType.MonsterAttack.rawValue && contact.bodyB.categoryBitMask == BodyType.Player.rawValue {
             
             print("Player Attack")
             
