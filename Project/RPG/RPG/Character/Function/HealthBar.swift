08d91c0acc067638184f5723c403bc935405c77b
everytime79 <everytime79@naver.com>
2021-10-11 16:49:42 +0900

    Update HealthBar.swift

--- Project/RPG/RPG/Character/Function/HealthBar.swift
+++ Project/RPG/RPG/Character/Function/HealthBar.swift
@@ -9,6 +9,26 @@ extension Character {
         HealthBar.zPosition = 2
         HealthBar.position = CGPoint(x: 0, y: self.size.height / 2 + 10)
         self.addChild(HealthBar)
+        
+        // Health
+        var Health = SKSpriteNode()
+        
+        if self.Sort == "Player" {
+            
+            Health = SKSpriteNode(imageNamed: "HP_Player")
+
+        } else if self.Sort == "Monster" {
+            
+            Health = SKSpriteNode(imageNamed: "HP_Monster")
+            
+        }
+        
+        Health.zPosition = 3
+        Health.position = CGPoint(x: -Health.size.width / 2, y: self.size.height / 2 + 10)
+        Health.name = "HP"
+        Health.anchorPoint = CGPoint(x: 0, y: 0.5)
+        self.addChild(Health)
+        
     }
     
 }
