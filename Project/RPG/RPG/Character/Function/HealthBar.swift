9f19a5ffc82b86eda4e0934a2b6e503349c1bdcf
everytime79 <everytime79@naver.com>
2021-10-11 16:46:53 +0900

    Update HealthBar.swift

--- Project/RPG/RPG/Character/Function/HealthBar.swift
+++ Project/RPG/RPG/Character/Function/HealthBar.swift
@@ -6,6 +6,9 @@ extension Character {
     func Function_Healthbar() {
         
         let HealthBar = SKSpriteNode(imageNamed: "HPBar")
+        HealthBar.zPosition = 2
+        HealthBar.position = CGPoint(x: 0, y: self.size.height / 2 + 10)
+        self.addChild(HealthBar)
     }
     
 }
