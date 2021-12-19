f131a2e02009b8265c8cd9657aea1f30a624b2df
everytime79 <everytime79@naver.com>
2021-12-19 17:19:00 +0900

    Update Data.swift

--- Project/RPG/RPG/GameScene/Function/Data.swift
+++ Project/RPG/RPG/GameScene/Function/Data.swift
@@ -38,67 +38,67 @@ extension GameScene {
 
     }
 // Player_End
-    // MARK: - Monster
-        func Data_Monster() {
+// MARK: - Monster
+    func Data_Monster() {
+        
+        let MonsterGroupData = CurrentMapData["MonsterGroup"] as? [String:Any] ?? [String:Any]()
+        
+        var Number = Int(0)
+        for (_, value) in MonsterGroupData {
             
-            let MonsterGroupData = CurrentMapData["MonsterGroup"] as? [String:Any] ?? [String:Any]()
+            let Data = value as! [String:Any]
+            let PositionX = Data["PositionX"] as! CGFloat
+            let PositionY = Data["PositionY"] as! CGFloat
+            let Sort = Data["Sort"] as! String
+            let AT = Data["AT"] as! CGFloat
+            let DF = Data["DF"] as! CGFloat
+            let Luck = Data["Luck"] as! CGFloat
+            let Class = Data["Class"] as! String
+            let Costume = Data["Costume"] as! String
+            let SP = Data["SP"] as! CGFloat
+            let HP = Data["HP"] as! CGFloat
             
-            var Number = Int(0)
-            for (_, value) in MonsterGroupData {
-                
-                let Data = value as! [String:Any]
-                let PositionX = Data["PositionX"] as! CGFloat
-                let PositionY = Data["PositionY"] as! CGFloat
-                let Sort = Data["Sort"] as! String
-                let AT = Data["AT"] as! CGFloat
-                let DF = Data["DF"] as! CGFloat
-                let Luck = Data["Luck"] as! CGFloat
-                let Class = Data["Class"] as! String
-                let Costume = Data["Costume"] as! String
-                let SP = Data["SP"] as! CGFloat
-                let HP = Data["HP"] as! CGFloat
-                
-                let EXP = Data["EXP"] as! CGFloat
+            let EXP = Data["EXP"] as! CGFloat
 
-                let SearchRange = Data["SearchRange"] as! CGFloat
-                let AttackRange = Data["AttackRange"] as! CGFloat
-                
-    // MARK: - Monster.
-                Monster = Character(imageNamed: "Archer")
-                Monster.PositionX = PositionX
-                Monster.PositionY = PositionY
-                Monster.Sort = Sort
-                Monster.Class = Class
-                Monster.Costume = Costume
-                Monster.AT = AT
-                Monster.DF = DF
-                Monster.Luck = Luck
-                Monster.SP = SP
-                Monster.RunSP = SP
-                Monster.HP = HP
-                Monster.CurrentHP = HP
-                Monster.EXP = EXP
-                
-                Monster.SearchRange = SearchRange
-                Monster.AttackRange = AttackRange
-                Monster.Number = Number
-                
-                Monster.position = CGPoint(x: PositionX, y: PositionY)
-                Monster.zPosition = 10
-                Monster.Function_HealthBar()
-                Monster.PhysicsBody_Monster() 
-                
-                MonsterGroup.append(Monster)
-                self.addChild(Monster)
-                
-                MonsterMinion = Character(color: UIColor.red, size: CGSize(width: 5, height: 5))
-                MonsterMinion.Number = Number
-                MonsterMinionGroup.append(MonsterMinion)
-                MiniMap.addChild(MonsterMinion)
-                
-                Number += 1 // 각각의 몬스터는 고유의 번호를 가지고 있다.
-                
-            }
+            let SearchRange = Data["SearchRange"] as! CGFloat
+            let AttackRange = Data["AttackRange"] as! CGFloat
+            
+// MARK: - Monster.
+            Monster = Character(imageNamed: "Archer")
+            Monster.PositionX = PositionX
+            Monster.PositionY = PositionY
+            Monster.Sort = Sort
+            Monster.Class = Class
+            Monster.Costume = Costume
+            Monster.AT = AT
+            Monster.DF = DF
+            Monster.Luck = Luck
+            Monster.SP = SP
+            Monster.RunSP = SP
+            Monster.HP = HP
+            Monster.CurrentHP = HP
+            Monster.EXP = EXP
+            
+            Monster.SearchRange = SearchRange
+            Monster.AttackRange = AttackRange
+            Monster.Number = Number
+            
+            Monster.position = CGPoint(x: PositionX, y: PositionY)
+            Monster.zPosition = 10
+            Monster.Function_HealthBar()
+            Monster.PhysicsBody_Monster() 
+            
+            MonsterGroup.append(Monster)
+            self.addChild(Monster)
+            
+            MonsterMinion = Character(color: UIColor.red, size: CGSize(width: 5, height: 5))
+            MonsterMinion.Number = Number
+            MonsterMinionGroup.append(MonsterMinion)
+            MiniMap.addChild(MonsterMinion)
+            
+            Number += 1 // 각각의 몬스터는 고유의 번호를 가지고 있다.
+            
         }
-    // Monster_End
+    }
+// Monster_End
 }
