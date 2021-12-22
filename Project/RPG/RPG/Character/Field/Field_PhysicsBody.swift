a4f7a5e6193f40bf379365f39486780ff75a893b
everytime79 <everytime79@naver.com>
2021-12-22 23:11:30 +0900

    Update Field_PhysicsBody.swift

--- Project/RPG/RPG/Character/Field/Field_PhysicsBody.swift
+++ Project/RPG/RPG/Character/Field/Field_PhysicsBody.swift
@@ -5,17 +5,3 @@ extension Field {
     
     
     //12. 19
-// MARK: - Portal
-    func PhysicsBody_Portal() {
-    
-        let PhysicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 25, height: 25))
-        
-        self.physicsBody = PhysicsBody
-        self.physicsBody?.isDynamic = true
-        self.physicsBody?.affectedByGravity = false // 중력의 영향 x
-        self.physicsBody?.allowsRotation = false // 마주쳤을 떄 돌 필요가 없다.
-        self.physicsBody?.categoryBitMask = BodyType.Portal.rawValue // Player = 1, Monster = 2
-        self.physicsBody?.contactTestBitMask = BodyType.Player.rawValue
-        self.physicsBody?.collisionBitMask = 0
-    }
-}
