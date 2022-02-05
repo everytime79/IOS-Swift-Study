6e3c33ebfa9b8e4c12ca5193948fdbad5c1a8b90
everytime79 <everytime79@naver.com>
2022-02-06 01:16:37 +0900

    Update Item.swift

--- Project/RPG/RPG/Character/Function/Item.swift
+++ Project/RPG/RPG/Character/Function/Item.swift
@@ -1,1 +1,23 @@
+import SpriteKit
+import GameplayKit
 
+extension Character {
+        
+    func Function_Item(){
+        
+        let ItemBody = Character(imageNamed: "ItemButton")
+        ItemBody.zPosition = 1
+        ItemBody.position = CGPoint(x: 0, y: self.size.height) // 캐릭터 머리 위에서 사용
+        ItemBody.xScale = 0.7
+        ItemBody.yScale = 0.7
+        
+        self.addChild(ItemBody)
+        
+        // Action //
+        let FadeOutAction = SKAction.fadeOut(withDuration: 1)
+        let RemoveAction = SKAction.run { ItemBody.removeFromParent() }
+        
+        ItemBody.run(SKAction.sequence([FadeOutAction, RemoveAction]))
+        
+    }
+}
