7ae0bc90f9f59b04107e4f09296773747f031fc1
everytime79 <everytime79@naver.com>
2022-03-27 23:00:51 +0900

    Update GameViewController.swift

--- Project/RPG/RPG/GameViewController.swift
+++ Project/RPG/RPG/GameViewController.swift
@@ -10,12 +10,12 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-// MARK: - Status Bar
-    override var prefersStatusBarHidden: Bool { return true } // 상단 스테이스 바를 숨길 건인지 여부
-// <- Status Bar
     
-// MARK: - Rotaion & Orientation
+    // MARK: - Status Bar
+    override var prefersStatusBarHidden: Bool { return true } // 상단 스테이스 바를 숨길 건인지 여부
+    // <- Status Bar
+    
+    // MARK: - Rotaion & Orientation
     override var shouldAutorotate: Bool { return true } // 자동으로 로테이트 할 것인지 여부
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -25,9 +25,9 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
-// <- Rotaion & Orientation
-
+    
+    // <- Rotaion & Orientation
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,7 +35,7 @@ class GameViewController: UIViewController {
         Scene = GameScene(fileNamed: CurrentMap)!
         Scene.CurrentMap = CurrentMap
         Scene.scaleMode = .aspectFill // 화면 꽉차게
-
+        
         
         let View = self.view as! SKView
         View.presentScene(Scene)
