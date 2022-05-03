7a161fdb3ea1eab262dd699b62063d69235d5a6d
everytime79 <everytime79@naver.com>
2022-05-04 01:59:18 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -41,7 +41,10 @@ class Character: SKSpriteNode {
     var DeltaX = CGFloat() // 플레이어의 위치를 계산하기 위한 좌표값
     var DeltaY = CGFloat()
     var AdjustedPosition = CGPoint()
-
+    
+    // Move //
+    var SpeedX = CGFloat()
+    var SpeedY = CGFloat()
     
     // Mode //
     var MoveDelayIs = Bool(false) // 값이 2개. 참 거짓 트루 폴스-> 딜레이가 있는가 없는가,
