12474dc6c831eb1bb8283052098e6cf19460ad93
everytime79 <everytime79@naver.com>
2022-05-12 01:22:23 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -5,12 +5,7 @@ class Character: SKSpriteNode {
     
 // MARK: - Variable
     
-    // Direction //
-    var CurrentDirection = Direction.ST
-    var LastDirection = Direction.ST
-    
-    //공격에 대한 방향
-    var AttackDirection = Direction.S
+   
     
     // Status //
     var PositionX = CGFloat()
