cac54db9ffa9aceacf9c6d09bd08741b74cb52a9
everytime79 <everytime79@naver.com>
2022-05-16 09:00:42 +0900

    Update GameViewController.swift

--- Project/RPG/RPG/GameViewController.swift
+++ Project/RPG/RPG/GameViewController.swift
@@ -39,7 +39,8 @@ class GameViewController: UIViewController {
         let View = self.view as! SKView
         View.presentScene(Scene)
         View.ignoresSiblingOrder = true
-
+        View.showsFPS = true
+        View.showsNodeCount = true
     }
 }
 
