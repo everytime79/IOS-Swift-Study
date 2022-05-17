a0ed66600451d62075cbd059038ac162c17c56a8
everytime79 <everytime79@naver.com>
2022-05-17 09:01:21 +0900

    Update Move.swift

--- Project/RPG/RPG/Character/Function/Move.swift
+++ Project/RPG/RPG/Character/Function/Move.swift
@@ -145,9 +145,7 @@ extension Character {
                 AttackDirection = CurrentDirection
                 
             }
-            
             LastDirection = CurrentDirection
-            
         }
         
         if self.MoveDelayIs == false { // 만약 딜레이가 없다면, 그때 움직임 true로 바뀌면 안움직임
