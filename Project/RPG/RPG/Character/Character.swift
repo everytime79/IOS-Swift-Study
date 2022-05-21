9401a9d3f98890a35788a26a1d5d2892639ba2d5
everytime79 <everytime79@naver.com>
2022-05-21 19:00:49 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -12,7 +12,25 @@ class Character: SKSpriteNode {
     //공격에 대한 방향
     var AttackDirection = Direction.S
     
-   
+    // Status //
+    var PositionX = CGFloat()
+    var PositionY = CGFloat()
+    var Sort = String()
+    var Costume = String()
+    var Class = String()
+    
+    var AT = CGFloat()
+    var DF = CGFloat()
+    var Luck = CGFloat()
+    
+    var SP = CGFloat()
+    var RunSP = CGFloat()
+    var WalkSP = CGFloat(1)
+    
+    var HP = CGFloat()
+    var CurrentHP = CGFloat()
+    var EXP = CGFloat()
+    var LV = CGFloat()
     
     var Gold = Int()
     var GoldAmount = Int()
