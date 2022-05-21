85b500126ef86a5e0d9257c829276d49a23e56f2
everytime79 <everytime79@naver.com>
2022-05-21 19:00:57 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -32,28 +32,7 @@ class Character: SKSpriteNode {
     var EXP = CGFloat()
     var LV = CGFloat()
     
-    var Gold = Int()
-    var GoldAmount = Int()
     
-    var Number = Int()
-    var SearchRange = CGFloat()
-    var AttackRange = CGFloat()
-    var DeltaX = CGFloat() // 플레이어의 위치를 계산하기 위한 좌표값
-    var DeltaY = CGFloat()
-    var AdjustedPosition = CGPoint()
-    
-    // Move //
-    var SpeedX = CGFloat()
-    var SpeedY = CGFloat()
-    
-    // Mode //
-    var MoveDelayIs = Bool(false) // 값이 2개. 참 거짓 트루 폴스-> 딜레이가 있는가 없는가,
-    var AttackDelayIs = Bool(false)
-    var AttackDamageIs = Bool(true)
-    var SkillDelayIs = Bool(false)
-    var StopDelayIs = Bool(false)
-    var WalkingIs = Bool(true)
-    var GoldActiveIs = Bool(true)
     
 // Variable_End
 }
