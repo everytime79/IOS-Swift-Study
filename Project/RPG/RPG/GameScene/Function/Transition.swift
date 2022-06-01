2923fc74124d0b569155050568a08f85047de88e
everytime79 <everytime79@naver.com>
2022-06-02 00:10:00 +0900

    Update Transition.swift

--- Project/RPG/RPG/GameScene/Function/Transition.swift
+++ Project/RPG/RPG/GameScene/Function/Transition.swift
@@ -5,6 +5,12 @@ extension GameScene {
     
     func Function_Transition(Desination: String){
         
-
+        let Scene = GameScene(fileNamed: Desination)
+        Scene?.CurrentMap = Desination
+        Scene?.scaleMode = .aspectFill
+        
+        let Transition = SKTransition.fade(with: UIColor.black, duration: 1)
+        self.view?.presentScene(Scene!, transition: Transition)
+    }
     // 11:34
 }
