524dede0b9ee4f344b24e2a341374d311bb9f665
everytime79 <everytime79@naver.com>
2022-06-29 02:56:16 +0900

    Update GameViewController.swift

--- Project/RPG/RPG/GameViewController.swift
+++ Project/RPG/RPG/GameViewController.swift
@@ -35,7 +35,11 @@ class GameViewController: UIViewController {
         Scene.CurrentMap = CurrentMap
         Scene.scaleMode = .aspectFill // 화면 꽉차게
         
-       
+        let View = self.view as! SKView
+        View.presentScene(Scene)
+        View.ignoresSiblingOrder = true
+        View.showsFPS = true
+        View.showsNodeCount = true
     }
 }
 
