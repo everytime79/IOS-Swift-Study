a75d2897cdb8109ed56543112a867102bd3c3b09
everytime79 <everytime79@naver.com>
2022-07-05 00:14:48 +0900

    Update Move.swift

--- Project/RPG/RPG/Character/Function/Move.swift
+++ Project/RPG/RPG/Character/Function/Move.swift
@@ -12,31 +12,7 @@ extension Character {
             SpeedY = 0
             
             if CurrentDirection != LastDirection {
-                
-                removeAction(forKey: "Move")
-                // 애니메이션 삭제 후,
-                
-                if LastDirection == Direction.E {
-                    
-                    self.texture = SKTexture(imageNamed: self.Costume + "MoveE1")
-                }
-                
-                if LastDirection == Direction.W {
-                    
-                    self.texture = SKTexture(imageNamed: self.Costume + "MoveW1")
-                }
-                
-                if LastDirection == Direction.S {
-                    
-                    self.texture = SKTexture(imageNamed: self.Costume + "MoveS1")
-                }
-                
-                if LastDirection == Direction.N {
-                    
-                    self.texture = SKTexture(imageNamed: self.Costume + "MoveN1")
-                } //최종 방향에 해당하는 곳으로 쳐다보게 한다.
-                
-                AttackDirection = LastDirection
+           
             }
             
             LastDirection = CurrentDirection
