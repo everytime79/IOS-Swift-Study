7916a495e22bb6a7e995e3de740b177818d55720
everytime79 <everytime79@naver.com>
2022-07-27 14:37:27 +0900

    Update Character.swift

--- Project/RPG/RPG/Character/Character.swift
+++ Project/RPG/RPG/Character/Character.swift
@@ -19,7 +19,18 @@ class Character: SKSpriteNode {
     var Costume = String()
     var Class = String()
     
-    \
+    var AT = CGFloat()
+    var DF = CGFloat()
+    var Luck = CGFloat()
+    
+    var SP = CGFloat()
+    var RunSP = CGFloat()
+    var WalkSP = CGFloat(1)
+    
+    var HP = CGFloat()
+    var CurrentHP = CGFloat()
+    var EXP = CGFloat()
+    var LV = CGFloat()
     
     var Gold = Int()
     var GoldAmount = Int()
